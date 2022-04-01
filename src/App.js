9eae167cc64c7fa7743c1d65cc44ac69c6164b0d
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2022-04-01 11:08:56 +0200

    seach box and footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="img-logo" alt="Shecodes logo" />
+        <form className="searchengine">
+          <input className='inputbox' type="search" />
+         
+        </form>
+        <footer>
+          Coded by Gagi Behrh
+        </footer>
       </header>
     </div>
   );
