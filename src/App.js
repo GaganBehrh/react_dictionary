7017221aa8aee48b324b8cc9820c85ed237aaf2b
Gagan Behrh <gaganpreet.behrh3@gmail.com>
2022-04-01 11:46:31 +0200

    functonality to the input box added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.png';
+import React, { useState } from "react";
+import logo from "./logo.png";
 import "./App.css";
+
 function App() {
+  let [keyword, setKeyword] = useState("");
+  function handlechange(event) {
+    event.preventDefault();
+    setKeyword(event.target.value);
+    console.log(event.target.value);
+  }
+  function Search(event) {
+    event.preventDefault();
+    alert("Searching");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="img-logo" alt="Shecodes logo" />
-        <form className="searchengine">
-          <input className='inputbox' type="search" />
-         
+        <form className="searchengine" onSubmit={Search}>
+          <input className="inputbox" type="search" onChange={handlechange} />
         </form>
-        <footer>
-          Coded by Gagi Behrh
-        </footer>
+        <footer>Coded by Gagi Behrh</footer>
       </header>
     </div>
   );
